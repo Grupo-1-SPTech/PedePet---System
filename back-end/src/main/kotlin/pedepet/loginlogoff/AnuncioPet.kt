4b91023856a0b7f926de6d9ce1b360f058879d5f
notfabo <fabio.seabra@sptech.school>
2023-04-18 22:02:23 -0300

    um pouco do cadastro

---
+++ back-end/src/main/kotlin/pedepet/loginlogoff/AnuncioPet.kt
@@ -0,0 +1,7 @@
+package pedepet.loginlogoff
+
+import javax.validation.constraints.NotBlank
+
+data class AnuncioPet {
+    @field:NotBlank
+}
--- back-end/src/main/kotlin/pedepet/loginlogoff/Usuario.kt
+++ back-end/src/main/kotlin/pedepet/loginlogoff/Usuario.kt
@@ -1,10 +1,29 @@
 package pedepet.loginlogoff
 
+import org.hibernate.validator.constraints.br.CPF
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
+
 data class Usuario(
-    val login:String,
-    var senha:String,
-    val nome:String,
-    var ativo:Boolean = true,
-    var autenticado:Boolean = false
+
+        // PARTE 1 CADASTRO
+        @field:NotBlank @field:Size(min = 3, max = 45) var nome:String,
+        @field:Email var email:String,
+        @field:CPF var cpf:String,
+        @field:Pattern(
+                regexp = "(\\(?\\d{2}\\)?\\s)?(\\d{4,5}\\-\\d{4})",
+                message = "Envie um telefone válido"
+        ) var telefone: String,
+        @field:NotBlank var senha:String,
+        @field:NotBlank var tipoUsuario:Int = 1, // COMPRADOR = 1, VENDEDOR = 2
+        //var ativo:Boolean = true,
+        //var autenticado:Boolean = false
+
+        // PARTE 2 CADASTRO
+        @field:NotBlank var interesse:Int = 1, // COMPRAR PET = 1, VENDER UM PET = 2
+        @field:NotBlank var preferenciaRaca:Int = 1, // SIM = 1, NÃO = 2
+        @field:NotBlank var pretendeEsperar:
 ) {
 }
