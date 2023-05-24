f4c6230c696b930268c7c788e96ed2c6f38a9f00
RuyBarbosa22 <ruy.castro@sptech.school>
2023-05-23 21:10:40 -0300

    Corrigindo dropdown de contato

--- Site/public/js/index.js
+++ Site/public/js/index.js
@@ -1,3 +1,4 @@
+// função select ASSUNTO
 const optionMenu = document.querySelector('.select-menu');
     selectBtn = optionMenu.querySelector('.select-btn');
     options = optionMenu.querySelectorAll('.option');
@@ -9,9 +10,28 @@ options.forEach(option => {
     option.addEventListener('click', () =>{
         let selectedOption = option.querySelector(".option-text").innerText;
         sBtn_text.innerText = selectedOption;
-        console.log(selectedOption);
+        console.log(selectedOption); // valor selecionado pelo usuário
 
         optionMenu.classList.remove('active')
+    })
+});
+
+
+// função select ESTADOS
+const optionMenu2 = document.querySelector('.select-menu2');
+    selectBtn2 = optionMenu2.querySelector('.select-btn2');
+    options2 = optionMenu2.querySelectorAll('.option2');
+    sBtn_text2 = optionMenu2.querySelector('.sBtn-text2');
+
+selectBtn2.addEventListener('click', () => optionMenu2.classList.toggle('active2'))
+
+options2.forEach(option2 => {
+    option2.addEventListener('click', () =>{
+        let selectedOption2 = option2.querySelector(".option-text2").innerText;
+        sBtn_text2.innerText = selectedOption2;
+        console.log(selectedOption2); // valor selecionado pelo usuário
+
+        optionMenu2.classList.remove('active2')
     })
 });
 
