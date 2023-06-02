d753a8fd77031195ea66cca45062bd0d4d826127
vitor.macauba04 <vitor.macauba@sptech.school>
2023-06-02 13:22:11 -0300

    teste js login

--- Site/public/js/login.js
+++ Site/public/js/login.js
@@ -1,4 +1,8 @@
-function entrar(){
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn-entrar").addEventListener("click", entrar);
+});
+
+function entrar() {
     fetch("http://localhost:3000/usuarios/login", {
         method: "POST",
         headers: {
@@ -9,11 +13,14 @@ function entrar(){
             senha: document.getElementById("input_senha").value
         })
     })
-    .then(res => res.json())
-    .then((res) => {
-        console.log(res)
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+        .then(res => res.json())
+        .then((res) => {
+            console.log(res);
+            // Faça algo com a resposta do servidor, como redirecionar para outra página
+            // ou exibir uma mensagem de sucesso.
+        })
+        .catch((err) => {
+            console.log(err);
+            // Trate o erro, por exemplo, exibindo uma mensagem de erro para o usuário.
+        });
 }
