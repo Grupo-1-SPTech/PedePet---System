d85fa39dbd41273f1abda77f5c2e29a2ad4337cc
= <fabio.seabra@sptech.school>
2023-08-26 12:43:47 -0300

    tema salvo no localStorage

--- Site/public/js/puppys_ad_btn_perfil.js
+++ Site/public/js/puppys_ad_btn_perfil.js
@@ -103,3 +103,30 @@ function defineCurrentTheme(theme) {
 function entrar() {
     window.location.href = "./login.html"
 }
+
+// tema ficar no localStorage
+
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+//store theme
+const storeTheme = function(theme) {
+    localStorage.setItem("theme", theme);
+}
+
+const setTheme = function() {
+    const activeTheme = localStorage.getItem("theme");
+    colorThemes.forEach((themeOption) => {
+        if(themeOption.id === activeTheme){
+            themeOption.checked = true;
+        }
+    });
+    document.documentElement.className = theme;
+};
+
+colorThemes.forEach(themeOption => {
+    themeOption.addEventListener('click', () => {
+        storeTheme(themeOption.id);    
+    });
+});
+
+document.onload = setTheme();
