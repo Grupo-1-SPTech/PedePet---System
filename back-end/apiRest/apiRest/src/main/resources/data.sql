ebee26ff519496d1d35272b5489e7deac98c2ade
notfabo <fabio.seabra@sptech.school>
2023-09-11 19:53:35 -0300

    arrumando algumas coisas do front, ajustando dark mode e att modelagem de dados

--- back-end/apiRest/apiRest/src/main/resources/data.sql
+++ back-end/apiRest/apiRest/src/main/resources/data.sql
@@ -16,7 +16,7 @@ values
 
 insert into ninhada (titulo, raca_mae, idade_mae, porte_mae, pedigree_mae, vacina_mae, raca_pai, idade_pai, porte_pai, pedigree_pai, vacina_pai, foto_pet, visualizacoes, qtd_filhotes, descricao, fk_usuario)
 values
-('Lulu da pomerânia os mais queridos','Pomerânia (Spitz Alemão)', 5, 'Pequeno', 1, 1, 'Pomerânia (Spitz Alemão)', 4, 'Pequeno', 1, 1,'https://canilfittipaldi.com.br/Spitz-Alemão/spitz-03.jpg', 1, 6, '6 lindos filhotes', 1),
+('Lulu da pomerânia os mais queridos','Spitz Alemão', 5, 'Pequeno', 1, 1, 'Pomerânia (Spitz Alemão)', 4, 'Pequeno', 1, 1,'https://canilfittipaldi.com.br/Spitz-Alemão/spitz-03.jpg', 1, 6, '6 lindos filhotes', 1),
 ('Corgi o cachoro que anda rebolando','Corgi', 5, 'Pequeno', 1, 1, 'Corgi', 4, 'Pequeno', 1, 1,'https://img.olx.com.br/images/83/835384198879135.webp', 1, 2, '2 lindos filhotes', 1),
 ('Os mais fofos Samoiedas','Samoieda', 8, 'Grande', 1, 1, 'Samoieda', 9, 'Grande', 1, 1,'https://gestoranimal.com.br/uploads/2019/12/content/pequeno-filhote-de-samoieda.jpg', 1, 1, '1 lindos filhotes', 3),
 ('Lindos Shibas','Shiba Inu', 8, 'Grande', 1, 1, 'Shiba Inu', 9, 'Grande', 1, 1,'https://centralparkpuppies.com/wp-content/uploads/2022/06/shiba-inu-dogs.png', 1, 3, '3 lindos filhotes', 3);
